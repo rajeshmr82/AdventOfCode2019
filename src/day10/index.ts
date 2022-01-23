f72fadc8e9bda6320742e5e5b4efe464085e1a64
Rajesh M R <rajesh.mr82@gmail.com>
2022-01-23 10:47:27 +0530

    Day 10

---
+++ src/day10/index.ts
@@ -0,0 +1,183 @@
+import run from "aocrunner";
+import { count } from "console";
+import { stat } from "fs";
+
+const parseInput = (rawInput: string) => rawInput;
+
+class Location {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const parseAsteriods = (input: string[]) => {
+  let asteroids: Location[] = [];
+  for (let i = 0; i < input.length; i++) {
+    const row = input[i];
+    for (let j = 0; j < row.length; j++) {
+      if (input[i][j] == "#") {
+        asteroids.push(new Location(j, i));
+      }
+    }
+  }
+  return asteroids;
+};
+
+const getAngle = (a: Location, b: Location) => {
+  let angle =
+    ((Math.atan2(b.y - a.y, b.x - a.x) * 180) / Math.PI + 90 + 360) % 360;
+  return {
+    asteroid: b,
+    angle: angle,
+    distance: Math.hypot(a.x - b.x, a.y - b.y),
+  };
+};
+
+const getVisibleAsteriods = (
+  asteroid: Location,
+  asteroids: Location[],
+): any => {
+  let allOthers = asteroids
+    .map((o) => getAngle(asteroid, o))
+    .filter((o) => o.distance !== 0);
+  allOthers.sort((a, b) => a.distance - b.distance);
+
+  for (let i = 0; i < allOthers.length; i++) {
+    const curr = allOthers[i];
+    if (curr == undefined) continue;
+    allOthers = allOthers.filter(
+      (o) =>
+        o.angle !== curr.angle ||
+        (o.angle === curr.angle && o.distance === curr.distance),
+    );
+  }
+
+  return {
+    asteroid: asteroid,
+    //others: allOthers,
+    count: allOthers.length,
+  };
+};
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput).split("\n");
+  let asteroids: Location[] = parseAsteriods(input);
+
+  const visibleCount = asteroids.map((asteroid) => {
+    return getVisibleAsteriods(asteroid, asteroids);
+  });
+
+  return Math.max.apply(
+    Math,
+    visibleCount.map(function (o) {
+      return o.count;
+    }),
+  );
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput).split("\n");
+  let asteroids: Location[] = parseAsteriods(input);
+
+  const visibleAsteroids = asteroids.map((asteroid) => {
+    return getVisibleAsteriods(asteroid, asteroids);
+  });
+
+  visibleAsteroids.sort((a, b) => b.count - a.count);
+  const station = visibleAsteroids[0].asteroid;
+
+  let others = asteroids
+    .filter((a) => !(a.x === station.x && a.y === station.y))
+    .map((o) => getAngle(station, o));
+
+  others.sort((a, b) => {
+    if (a.angle === b.angle) {
+      return a.distance - b.distance;
+    }
+    return a.angle - b.angle;
+  });
+
+  let count = 0,
+    angle = 0,
+    result;
+
+  while (count < 200) {
+    let index = others.findIndex((a) => a.angle >= angle);
+    let { asteroid } = others[index];
+    result = asteroid.x * 100 + asteroid.y;
+    others.splice(index, 1);
+
+    angle = others.find((a) => a.angle > angle)?.angle || 0;
+    count++;
+  }
+
+  return result;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+        `,
+        expected: 210,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+        .#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+        `,
+        expected: 802,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
